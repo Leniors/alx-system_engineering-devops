943c5440f1598f6f3226a1aa6c4dca0616f84c2b
Leniors <mokualeeroy@gmail.com>
2024-01-20 02:51:41 +0300

    Install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,7 +5,7 @@ package { 'python3-pip':
 }
 
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask=2.1.0',
+  command => 'pip3 install flask=2.1.0',
   path    => ['/bin', '/usr/bin']
   unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }
