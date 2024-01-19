366dcacdb8062b854098c5496cd69efd6c7efe42
Leniors <mokualeeroy@gmail.com>
2024-01-20 01:57:27 +0300

    create file using puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# create a file school in tmp directory
+
+file { '/tmp/school':
+  ensure  => 'present',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
