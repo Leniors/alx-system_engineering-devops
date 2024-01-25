68102be3e097f685862ca8bd4635999c300f1168
Leniors <mokualeeroy@gmail.com>
2024-01-25 04:00:45 +0300

    install package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,7 +5,8 @@ package { 'python3-pip':
 }
 
 exec { 'install_flask':
-  command => '/usr/bin/pip install flask==2.1.0',
+  command => '/usr/bin/pip3 install flask==2.1.0',
   path    => ['/bin', '/usr/bin']
   unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+  require => Package['python3-pip'],
 }
