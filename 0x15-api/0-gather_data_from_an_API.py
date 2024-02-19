06a9d43f3047babfaa2d66868eb779b4dc27b69f
Leniors <mokualeeroy@gmail.com>
2024-02-19 23:27:31 +0300

    gather_data_from_an_API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+request an api
+"""
+import json
+import random
+import requests
+import sys
+
+user_url = f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}"
+user_json = requests.get(user_url)
+user = json.loads(user_json.text)
+
+todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={sys.argv[1]}"
+todos_json = requests.get(todo_url)
+todos = json.loads(todos_json.text)
+
+done_todos = 0
+for todo in todos:
+    if todo.get("completed") == True:
+        done_todos += 1
+
+print(f"Employee {user['name']} is done with tasks({done_todos}/{len(todos)}):")
+
+for todo in todos:
+    if todo.get("completed") == True:
+        print(f"\t{todo.get('title')}")
