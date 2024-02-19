931f43c3f204a287564bb88c79a0404c30cb41ce
Leniors <mokualeeroy@gmail.com>
2024-02-20 00:55:13 +0300

    json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+request an api
+"""
+
+if __name__ == "__main__":
+    import json
+    import random
+    import requests
+    import sys
+
+    u_id = sys.argv[1]
+    user_url = f"https://jsonplaceholder.typicode.com/users/{u_id}"
+    user = requests.get(user_url).json()
+
+    todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={u_id}"
+    todos = requests.get(todo_url).json()
+
+    json_file = f"{user['id']}.json"
+
+    dic = {str(user['id']): todos}
+
+    with open(json_file, mode='w', newline='') as file:
+        file.write(json.dumps(dic))
+        file.write('\n')
