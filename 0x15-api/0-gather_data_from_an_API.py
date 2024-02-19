73d63a5f3b7fa5971ac778683dc1ff627ba7d4be
Leniors <mokualeeroy@gmail.com>
2024-02-19 23:33:12 +0300

    gather_data_from_an_API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,26 +2,29 @@
 """
 request an api
 """
-import json
-import random
-import requests
-import sys
 
-user_url = f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}"
-user_json = requests.get(user_url)
-user = json.loads(user_json.text)
+if __name__ == "__main__":
+    import json
+    import random
+    import requests
+    import sys
 
-todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={sys.argv[1]}"
-todos_json = requests.get(todo_url)
-todos = json.loads(todos_json.text)
+    user_url = f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}"
+    user_json = requests.get(user_url)
+    user = json.loads(user_json.text)
 
-done_todos = 0
-for todo in todos:
-    if todo.get("completed") == True:
-        done_todos += 1
+    todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={sys.argv[1]}"
+    todos_json = requests.get(todo_url)
+    todos = json.loads(todos_json.text)
 
-print(f"Employee {user['name']} is done with tasks({done_todos}/{len(todos)}):")
+    done_todos = 0
+    for todo in todos:
+        if todo.get("completed") == True:
+            done_todos += 1
 
-for todo in todos:
-    if todo.get("completed") == True:
-        print(f"\t{todo.get('title')}")
+    print(
+        f"Employee {user['name']} is done with tasks({done_todos}/{len(todos)}):")
+
+    for todo in todos:
+        if todo.get("completed") == True:
+            print(f"\t{todo.get('title')}")
